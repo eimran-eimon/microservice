8a5a0896d30fdd6d71cbdafcb391596e828f76d9
Eimon <eimran@cokreates.com>
2020-03-23 14:43:48 +0600

    [config] necessary dependency added!

---
+++ src/main/java/com/cokreates/microservice/MicroserviceApplication.java
@@ -0,0 +1,18 @@
+package com.cokreates.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+public class MicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceApplication.class, args);
+	}
+	
+}
