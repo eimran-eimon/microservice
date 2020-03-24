cbc856e8d0c1386a032fa27c06c6c1b645370a31
eimran-eimon <mdeimranhossaineimon@gmail.com>
2020-03-24 12:49:34 +0600

    microservice one is complted.

--- src/main/java/com/cokreates/microservice/controller/ExchangeValueController.java
+++ src/main/java/com/cokreates/microservice/controller/ExchangeValueController.java
@@ -3,7 +3,6 @@ package com.cokreates.microservice.controller;
 
 import com.cokreates.microservice.database.model.ExchangeValue;
 import com.cokreates.microservice.database.repository.ExchangeValueRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/main/java/com/cokreates/microservice/database/model/ExchangeValue.java
+++ src/main/java/com/cokreates/microservice/database/model/ExchangeValue.java
@@ -1,7 +1,5 @@
 package com.cokreates.microservice.database.model;
 
-import org.hibernate.annotations.GeneratorType;
-
 import javax.persistence.*;
 import java.math.BigDecimal;
 
