d86fd466059591bf9b77f2b74ca9c8e9e1a2ba66
eimran-eimon <mdeimranhossaineimon@gmail.com>
2020-03-24 18:08:24 +0600

    [load balancing] ribbon added

---
+++ microservice_1/src/main/java/com/cokreates/microservice/MicroserviceApplication1.java
@@ -0,0 +1,14 @@
+package com.cokreates.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class MicroserviceApplication1 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceApplication1.class, args);
+	}
+
+}
--- microservice_2/src/main/java/com/cokreates/microservice_2/proxy/ExchangeConversionMultipleProxy.java
+++ microservice_2/src/main/java/com/cokreates/microservice_2/proxy/ExchangeConversionMultipleProxy.java
@@ -2,11 +2,13 @@ package com.cokreates.microservice_2.proxy;
 
 
 import com.cokreates.microservice_2.CurrencyConversionBean;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "exchange-service",url = "localhost:8000")
+@FeignClient(name = "exchange-service")
+@RibbonClient(name="exchange-service")
 public interface ExchangeConversionMultipleProxy {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
